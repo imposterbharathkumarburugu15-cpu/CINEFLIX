3c07b1aacc56a320921366fd9ab0d88ddae4037a
imposterbharathkumarburugu15-cpu <imposterbharathkumarburugu15@gmail.com>
2025-08-21 21:47:10 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import './App.css'
 
 const CineflixNavbar = () => {
   return (
     <Navbar expand="lg" className="cineflix-navbar">
       <Container>
-        <Navbar.Brand href="/" className="brand-logo">
+        <Navbar.Brand as={Link} to="/" className="brand-logo">
            CINEFLIX
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/movies">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/">Login</Nav.Link>
+            <Nav.Link as={Link} to="/movies">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
