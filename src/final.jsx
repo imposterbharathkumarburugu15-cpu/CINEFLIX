690206c6f39d6f20fb5685121b8de830daf922d1
imposterbharathkumarburugu15-cpu <imposterbharathkumarburugu15@gmail.com>
2025-12-06 02:02:50 +0530

    Refactor Final component and improve formatting

--- src/final.jsx
+++ src/final.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import L1 from "./scan.png";
-import './final.css'
-const Final =()=>{
-    return(
+import './final.css';
+
+const Final = () => {
+    return (
         <div>
-            <h1>YOUR TICKET IS CONFIRMED .THANK YOU FOR VISITING <i>CineFlix...</i></h1>
-            <img src={L1} ClassName="scan-container" alt="Qr-Code"/>
+            <h1>
+                YOUR TICKET IS CONFIRMED. THANK YOU FOR VISITING <i>CineFlix...</i>
+            </h1>
+            <div className="scan-container">
+                <img src={L1} alt="Qr-Code" />
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default Final;
